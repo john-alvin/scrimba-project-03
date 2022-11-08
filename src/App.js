3340e5a4b2ac647b04925b32ded8cf85e0fd6231
John Alvin <johnalvin.bonifacio@proton.me>
2022-11-09 05:51:08 +0800

    add component for hero and card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./index.css";
 import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import Card from "./component/Card";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
